83fc7bca712149bb23df9c0a3523d57d053b888d
Annie Rhodes <annierhodes17@gmail.com>
2024-08-26 14:41:18 +0000

    updated

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,14 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    feild_2 = models.IntegerField(default=42)
+    feild_3 = models.CharField(null=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
     
 class Comment(models.Model):
     post = models.ForeignKey(
